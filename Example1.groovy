b5b91d1a4c179233432ab38e44044c557cc2b399
Isabel <belondon2012@gmail.com>
2012-10-07 14:02:16 +0200

    Ejemplo1 del clon

---
+++ Example1.groovy
@@ -0,0 +1,17 @@
+println "Given a series of words, each on a separate line,"
+println "this program finds the length of the longest word."
+println "Please enter several sentences, one per line. "
+println "Finish with a blank line."
+max = 0
+String str = "."
+while (str.length() > 0) {
+  str = System.console().readLine()
+  if (str.length() > max) {
+    max = str.length()
+  }
+}
+if (max == 0) {
+    println "There were no words."
+} else {
+    println "The longest sentence was " + max + " characters long."
+}
